70724dcddc8e1f8dfd06ec422f2186c0bd5499ce
Lixc <a1441982580@163.com>
2021-04-15 13:32:22 +0800

    这是一次提交

--- app/src/main/java/com/example/lxc/wangyiyunmusic/services/MusicService.java
+++ app/src/main/java/com/example/lxc/wangyiyunmusic/services/MusicService.java
@@ -53,12 +53,12 @@ public class MusicService extends Service {
          * 监听音乐播放完成，停止服务
          */
         public void startMusic() {
-            /**
-             * 判断当前是否已经正在播放音乐
-             *          如果当前播放的音乐路径和需要播放的音乐路径一致，则直接执行helper.playMusic()方法
-             *          如果当前没有正在播放的音乐，或者当前播放的音乐和需要播放的音乐路径不一致，则先调用helper.setPath(path)方法,
-             *              再调用helper.playMusic()方法
-             */
+
+//              判断当前是否已经正在播放音乐
+//                       如果当前播放的音乐路径和需要播放的音乐路径一致，则直接执行helper.playMusic()方法
+//                       如果当前没有正在播放的音乐，或者当前播放的音乐和需要播放的音乐路径不一致，则先调用helper.setPath(path)方法,
+//                           再调用helper.playMusic()方法
+//
             if (helper.getCurrentPlayPath() != null && helper.getCurrentPlayPath().equals(mMusicModel.getPath())) {
                 helper.playMusic();
             } else {
@@ -97,11 +97,11 @@ public class MusicService extends Service {
     private void startForeground() {
         PendingIntent pendingIntent = PendingIntent.getActivity(
                 this, 0, new Intent(this, WelcomeActivity.class), PendingIntent.FLAG_CANCEL_CURRENT);
-        Notification notification = null;
+        Notification notification ;
 
-        /**
-         * Android 26以上Notification特性适配
-         */
+
+         // Android 26以上Notification特性适配
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = createNotificationChannel();
             notification = new Notification.Builder(this, channel.getId())
@@ -109,6 +109,7 @@ public class MusicService extends Service {
                     .setContentTitle(mMusicModel.getName())
                     .setContentText(mMusicModel.getAuthor())
                     .setContentIntent(pendingIntent)
+
                     .build();
             NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
             manager.createNotificationChannel(channel);
